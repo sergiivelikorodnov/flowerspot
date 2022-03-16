b31b85131751a98ceef8c3b3d44d3ed9ae8d8ed1
sergiivelikorodnov <sergii.velikorodnov@gmail.com>
2022-03-16 11:32:42 +0100

    add: routes template

---
+++ flowrspot-app/src/components/App/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import PageWrapper from '../PageWrapper/PageWrapper';
+
+function App(): JSX.Element {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Root} element={<PageWrapper />}>
+          {/* <Route path={AppRoute.Login} element={<Login />} />
+          <Route path={AppRoute.Favorites}
+            element={
+              <PrivateRoute>
+                <Favorites />
+              </PrivateRoute>
+            }
+          /> */}
+          {/* <Route path='*' element={<NotFound />}/> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
